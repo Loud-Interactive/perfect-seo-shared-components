50884a8efc4a145a5c243e233fb3bd8afde95cba
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2024-09-06 14:17:32 -0500

    added assets

---
+++ assets/Brands.ts
@@ -0,0 +1,120 @@
+import { Brand, BrandStatus } from "./types";
+
+export const Brands: Array<Brand> = [
+  {
+    title: 'classifyPerfect.ai',
+    url: 'https://classifyPerfect.ai',
+    logo: '/svg/classifyPerfect.svg',
+    icon: '/svg/classifyPerfect-icon.svg',
+    status: BrandStatus.PLANNED,
+    primary: "#ffd900",
+  },
+  {
+    title: 'contentPerfect.ai',
+    url: 'https://contentPerfect.ai',
+    logo: '/svg/contentPerfect.svg',
+    icon: '/svg/contentPerfect-icon.svg',
+    status: BrandStatus.LIVE,
+    summary: "contentPerfect.AI leverages AI to deliver SEO-optimized content plans and calculated blog posts, boosting site traffic with data-driven strategies designed by SEO experts.",
+    primary: "#FEB800",
+    stagingUrl: "https://content-perfect-theta.vercel.app/"
+  },
+  {
+    title: 'factcheckPerfect.ai',
+    url: 'https://factcheckPerfect.ai',
+    logo: '/svg/factcheckPerfect.svg',
+    icon: '/svg/factcheckPerfect-icon.svg',
+    status: BrandStatus.COMING_SOON,
+    primary: "#99cc33",
+    summary: "factcheckPerfect.ai helps businesses verify their articles and content for accuracy and credibility using AI."
+  },
+  {
+    title: 'imagePerfect.ai',
+    url: 'https://imagePerfect.ai',
+    logo: '/svg/imagePerfect.svg',
+    icon: '/svg/imagePerfect-icon.svg',
+    status: BrandStatus.COMING_SOON,
+    summary: "Transform your blog into a highly engaging and SEO-optimized platform by generating a comprehensive blog strategy with strategic keywords and link structures.",
+    primary: "#EF476F",
+  },
+  {
+    title: 'indexPerfect.ai',
+    url: 'https://indexPerfect.ai',
+    logo: '/svg/indexPerfect.svg',
+    icon: '/svg/indexPerfect-icon.svg',
+    status: BrandStatus.COMING_SOON,
+    summary: "indexPerfect.ai helps businesses identify the effectiveness of their website's indexation using AI.",
+    primary: "#006400",
+  },
+  {
+    title: 'pagePerfect.ai',
+    summary: "Remove the guesswork and harness the power of AI for your SEO content. pagePerfect generates effective, optimized content that you simply copy and paste onto your website.",
+    url: 'https://pagePerfect.ai',
+    logo: '/svg/pagePerfect.svg',
+    icon: '/svg/pagePerfect-icon.svg',
+    status: BrandStatus.LIVE,
+    primary: "#666699",
+    stagingUrl: "https://page-perfect-one.vercel.app/"
+  },
+  {
+    title: 'perfectSEO.ai',
+    url: 'https://perfectSEO.ai',
+    logo: '/svg/perfectSEO.svg',
+    icon: '/svg/perfectSEO-icon.svg',
+    status: BrandStatus.MASTER,
+    stagingUrl: "https://perfect-seo-dash.vercel.app/",
+    primary: "#E63946"
+  },
+  {
+    title: 'preferencesPerfect.ai',
+    url: 'https://preferencesPerfect.ai',
+    logo: '/svg/preferencesPerfect.svg',
+    icon: '/svg/preferencesPerfect-icon.svg',
+    status: BrandStatus.LIVE,
+    primary: "#E63946",
+    summary: "preferencesPerfect allows you to update all your brand information across our AI suite all in one place."
+  },
+  {
+    title: 'profilePerfect.ai',
+    url: 'https://profilePerfect.ai',
+    logo: '/svg/profilePerfect.svg',
+    icon: '/svg/profilePerfect-icon.svg',
+    status: BrandStatus.PLANNED,
+    primary: "#235789",
+  },
+  {
+    title: 'redirectPerfect.ai',
+    url: 'https://redirectPerfect.ai',
+    logo: '/svg/redirectPerfect.svg',
+    icon: '/svg/redirectPerfect-icon.svg',
+    status: BrandStatus.PLANNED,
+    primary: "#999948",
+  },
+  {
+    title: 'socialPerfect.ai',
+    url: '/socialperfect',
+    logo: '/svg/socialPerfect.svg',
+    icon: '/svg/socialPerfect-icon.svg',
+    status: BrandStatus.LIVE,
+    summary: "Amplify your social media engagement by generating the right emoticon and hashtags to achieve the right tone",
+    primary: "#CC6699"
+  },
+  {
+    title: 'synopsisPerfect.ai',
+    url: 'https://synopsisPerfect.ai',
+    logo: '/svg/synopsisPerfect.svg',
+    icon: '/svg/synopsisPerfect-icon.svg',
+    status: BrandStatus.LIVE,
+    primary: "#8b59ad",
+    summary: "synopsisPerfect.ai helps businesses identify their brand and the effectiveness of information conveyed on their website using AI."
+  },
+  {
+    title: 'voicePerfect.ai',
+    url: 'https://voicePerfect.ai',
+    logo: '/svg/voicePerfect.svg',
+    icon: '/svg/voicePerfect-icon.svg',
+    status: BrandStatus.LIVE,
+    summary: "voicePerfect AI provides insights into the ideal tone, language style, common phrases, and inclusivity of your brand voice to perfect it in the minds of your customers.",
+    primary: "#6699cc"
+  }
+];

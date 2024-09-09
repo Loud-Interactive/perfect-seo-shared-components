e63eb1b3c25abe2cbcb56dea2473369e7653854e
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2024-09-09 13:45:24 -0500

    update layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -27,7 +27,8 @@ const Layout = ({ children, hideFooter, current, links, hasLogin = true, getCred
         <title>{current}</title>
         <link rel="icon" href={`/svg/${current.endsWith(".ai") ? current.replace(".ai", "") : current}-icon.svg`} />
         <meta charSet="UTF-8" />
-        <meta name="description" content={currentBrand.summary} />
+        <meta name="og:title" content={current} />
+        <meta name="og:description" content={currentBrand.summary} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous" />
       </Head>

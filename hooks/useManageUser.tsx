e009a027a25c82699fd8d42b7a13ba6a62e789e1
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2024-09-08 23:17:12 -0500

    update useManage

--- hooks/useManageUser.tsx
+++ hooks/useManageUser.tsx
@@ -2,7 +2,7 @@ import { StateTree } from "@/perfect-seo-shared-components/store/reducer";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from 'react'
 import { reduxReset, setAdmin, setLoading, setLoggedIn, setUser } from '@/perfect-seo-shared-components/store/actions'
-import { createClient } from '@/utils/supabase/components'
+import { createClient } from '@/perfect-seo-shared-components/utils/supabase/components'
 
 const useManageUser = (appKey) => {
   const { isLoggedIn, user, isAdmin, isLoading } = useSelector((state: StateTree) => state);

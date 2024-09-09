7fa616b2ccf56cefe95036e639afe546e8de3079
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2024-09-09 14:17:49 -0500

    upgrade package

--- components/MetaTags/MetaTags.tsx
+++ components/MetaTags/MetaTags.tsx
@@ -11,7 +11,10 @@ const MetaTags = ({ title, description, image, type, url }: MetaTagsProps) => {
   return (
     <Head>
       {title &&
-        <meta property="og:title" content={title} />
+        <>
+          <title>{title}</title>
+          <meta property="og:title" content={title} />
+        </>
       }
       {type &&
         <meta property="og:type" content={type} />
@@ -20,10 +23,36 @@ const MetaTags = ({ title, description, image, type, url }: MetaTagsProps) => {
         <meta property="og:description" content={description} />
       }
       {image &&
+
         <meta property="og:image" content={image} />
+
       }
       {url && <meta property="og:url" content={url} />}
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+
+      <meta name="description" content="ALS United" />
+
+      {/* <!-- Google / Search Engine Tags --> */}
+      {image && <meta itemProp="image" content={image} />}
+      {title && <meta itemProp="name" content={title} />}
+      {description && <meta itemProp="description" content={description} />}
+
+
+      {/* <!-- Facebook Meta Tags --> */}
+      {url && <meta property="og:url" content={url} />}
+      {type && <meta property="og:type" content={type} />}
+      {title && <meta property="og:title" content={title} />}
+      {description && <meta property="og:description" content={description} />}
+      {image && <meta property="og:image" content={image} />}
+
+      {/* <!-- Twitter Meta Tags --> */}
+      <meta name="twitter:card" content="summary_large_image" />
+      {title && <meta name="twitter:title" content={title} />}
+      {description && <meta name="twitter:description" content={description} />}
+      {image && <meta name="twitter:image" content={image} />}
+
+
+
       <meta charSet="UTF-8" />
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous" />
     </Head>

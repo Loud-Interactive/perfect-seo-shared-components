cb9b92d5a1bdead7f58539da43c3c3d754410d7b
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2024-09-09 10:54:28 -0500

    added special ai to layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -5,6 +5,8 @@ import style from './Layout.module.scss'
 import store from '@/perfect-seo-shared-components/store';
 import { Provider } from "react-redux";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { Brands } from '@/perfect-seo-shared-components/assets/Brands';
+import Head from 'next/head';
 
 
 interface LayoutProps extends React.HTMLProps<HTMLDivElement> {
@@ -17,8 +19,16 @@ interface LayoutProps extends React.HTMLProps<HTMLDivElement> {
 
 const Layout = ({ children, hideFooter, current, links, hasLogin = true, getCredits = false }: LayoutProps) => {
 
+  const currentBrand = Brands.find((brand) => brand.title === current)
   return (
     <>
+      <Head>
+        <title>{current}</title>
+        <link rel="icon" type="image/png" href={`/images/brands/${current.replace(".ai", "")}-icon.png`} />
+        <meta charSet="UTF-8" />
+        <meta name="description" content={currentBrand.summary} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Provider store={store}>
         <Header current={current} links={links} hasLogin={hasLogin} getCredits={getCredits} />
         <main className={style.wrap}>

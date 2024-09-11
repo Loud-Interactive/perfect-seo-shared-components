6ff14d1708d21c1bbaa6ca3e763847bc9d2fe603
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2024-09-11 15:28:32 -0500

    removed extras

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -3,10 +3,7 @@ import Footer from '../Footer/Footer'
 import Header from '../Header/Header'
 import style from './Layout.module.scss'
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { Brands } from '@/perfect-seo-shared-components/assets/Brands';
-import Head from 'next/head';
 import Script from 'next/script';
-import MetaTags from '../MetaTags/MetaTags';
 
 
 interface LayoutProps extends React.HTMLProps<HTMLDivElement> {
@@ -19,12 +16,8 @@ interface LayoutProps extends React.HTMLProps<HTMLDivElement> {
 
 const Layout = ({ children, hideFooter, current, links, hasLogin = true, getCredits = false }: LayoutProps) => {
 
-  const currentBrand = Brands.find((brand) => brand.title === current)
   return (
-    <><Head>
-      <link rel="icon" href={`/svg/${current.endsWith(".ai") ? current.replace(".ai", "") : current}-icon.svg`} />
-    </Head>
-      <MetaTags title={current} url={currentBrand?.url} description={currentBrand?.summary} image={`/svg/${current.endsWith(".ai") ? current.replace(".ai", "") : current}-icon.svg`} />
+    <>
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossOrigin="anonymous" />
       <Header current={current} links={links} hasLogin={hasLogin} getCredits={getCredits} />
       <main className={style.wrap}>

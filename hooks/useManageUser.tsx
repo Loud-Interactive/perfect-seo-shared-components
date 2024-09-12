4cf9168fc1a7aed4a44da6efad8b7dd9242daf33
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2024-09-12 13:31:32 -0500

    updated access

--- hooks/useManageUser.tsx
+++ hooks/useManageUser.tsx
@@ -11,67 +11,73 @@ const useManageUser = (appKey) => {
   const dispatch = useDispatch();
   const supabase = createClient()
 
-  useEffect(() => {
-    if (user) {
-      supabase
-        .from('profiles')
-        .select("*")
-        .eq('id', user.id)
-        .select()
-        .then(res => {
-          dispatch(setLoading(false))
-          console.log("get user", res.data)
-          if (res?.data && res?.data?.length > 0) {
-            if (res?.data[0]) {
-              console.log("set userData", res.data[0])
-              setUserData(res.data[0])
-              dispatch(setAdmin(res.data[0]?.admin))
-            }
+  const updateUser = (user) => {
+    supabase
+      .from('profiles')
+      .select("*")
+      .eq('id', user.id)
+      .select()
+      .then(res => {
+        dispatch(setLoading(false))
+
+        if (res?.data && res?.data?.length > 0) {
+          if (res?.data[0]) {
+
+            setUserData({ ...user, ...res.data[0] })
+            dispatch(setAdmin(res.data[0]?.admin))
           }
-        })
-    }
-    else {
-      dispatch(setLoading(false))
-    }
-  }, [user])
+        }
+      })
+  }
+
+
 
   useEffect(() => {
     if (userData) {
       if (!userData.email) {
         userData.email = user.email
       }
-      let domains = [userData?.email?.split("@")[1]]
-      if (userData?.user_metadata?.custom_claims) {
-        let customClaims = Object.keys(userData?.user_metadata.custom_claims).reduce((prev, curr) => {
-          if (userData?.user_metadata?.custom_claims[curr] !== domains[0]) {
-            return [...prev, userData?.user_metadata?.custom_claims[curr]]
-          }
-          else return prev
-        }, [])
-        domains = [...domains, ...customClaims].filter(obj => obj !== 'google' && obj !== "gmail")
-
-
-        let products = userData.products;
-        let key = appKey;
-        if (products) {
-          if (products[key]) {
-            products[key] = new Date().toISOString()
-          }
-          else {
-            products = { ...products, [key]: new Date().toISOString() }
-          }
+      let domains;
+      if (!userData.domains) {
+        domains = [userData?.email?.split("@")[1]]
+        if (userData?.user_metadata?.custom_claims) {
+          let customClaims = Object.keys(userData?.user_metadata.custom_claims).reduce((prev, curr) => {
+            if (userData?.user_metadata?.custom_claims[curr] !== domains[0]) {
+              return [...prev, userData?.user_metadata?.custom_claims[curr]]
+            }
+            else return prev
+          }, [])
+          domains = [...domains, ...customClaims].filter(obj => obj !== 'google' && obj !== "gmail")
         }
-        let profileObj: any = { meta_data: user, email: user.email, domains: domains, products: products };
-        profileObj.updated_at = new Date().toISOString()
-        supabase
-          .from('profiles')
-          .update(profileObj)
-          .eq('id', user.id)
-          .then(res => {
-            console.log(res)
-          }
-          )
+
       }
+      else {
+        domains = userData.domains
+      }
+
+
+
+
+
+      let products = userData.products;
+      let key = appKey;
+      if (products) {
+        if (products[key]) {
+          products[key] = new Date().toISOString()
+        }
+        else {
+          products = { ...products, [key]: new Date().toISOString() }
+        }
+      }
+      let profileObj: any = { meta_data: userData, email: userData.email, domains: domains, products: products };
+      profileObj.updated_at = new Date().toISOString()
+      dispatch(setUser({ ...userData, domains: domains }))
+      supabase
+        .from('profiles')
+        .update(profileObj)
+        .eq('id', userData.id)
+
+
     }
   }, [userData])
 
@@ -85,9 +91,8 @@ const useManageUser = (appKey) => {
           else if (res.error !== null) {
             dispatch(reset())
           } else {
-            console.log(res.data.user)
             dispatch(setLoggedIn(true))
-            return dispatch(setUser(res.data.user))
+            return updateUser(res.data.user)
           }
         })
     }

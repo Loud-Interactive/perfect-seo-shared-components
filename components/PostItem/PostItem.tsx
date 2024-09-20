67b8794bb002159977d9127b3ef65567fbcc4531
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2024-09-20 09:53:24 -0500

    fixed post item

--- components/PostItem/PostItem.tsx
+++ components/PostItem/PostItem.tsx
@@ -173,7 +173,7 @@ const PostItem = ({ post, refresh }) => {
           </div>
         </div>
         <div className="col-12 col-lg-6">
-          <div className='row g-0 d-flex'>
+          <div className='row g-2 d-flex justify-content-end'>
             <div className="col-12 d-flex justify-content-start mb-1">
               <strong className="text-primary me-2">Created on</strong> {moment(`${localPost?.created_at}Z`).local().format("dddd, MMMM Do, YYYY h:mma")}
             </div>

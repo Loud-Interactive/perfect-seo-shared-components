a8cc7715995b24e5eef029f2fe6fb0afd436dfb7
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2024-09-23 11:00:45 -0500

    cleanup

--- utils/supabase/actions.ts
+++ utils/supabase/actions.ts
@@ -3,16 +3,13 @@ import { useDispatch } from 'react-redux'
 
 export const login = (formData: any) => {
   const supabase = createClient()
-
   // type-casting here for convenience
   // in practice, you should validate your inputs
   const data = {
     email: formData.email as string,
     password: formData.password as string,
   }
-
   return supabase.auth.signInWithPassword(data)
-
 }
 
 export const signup = (formData: any) => {

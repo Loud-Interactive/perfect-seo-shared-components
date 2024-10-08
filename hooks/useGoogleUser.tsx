739748c3b497be0b03a54582b374f67ea7f0508b
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2024-10-08 14:30:43 -0500

    added response for now

--- hooks/useGoogleUser.tsx
+++ hooks/useGoogleUser.tsx
@@ -20,6 +20,10 @@ const useGoogleUser = (appKey) => {
   axios.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
+    supabase
+      .from('user_history')
+      .insert({ email: user.email, transaction_data: { ...response, dev_code: 'text' }, product: en.product })
+      .select('*')
     return response;
   }, function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger

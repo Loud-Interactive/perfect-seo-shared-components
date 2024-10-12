643f2ca8040c0032026d11f2a51732624b00315f
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2024-10-11 23:43:31 -0500

    changed old template

--- components/AdminPage/AdminPage.tsx
+++ components/AdminPage/AdminPage.tsx
@@ -1,11 +1,11 @@
-import { reduxReset } from "@/perfect-seo-shared-components/store/actions";
-import { StateTree } from "@/perfect-seo-shared-components/store/reducer";
+import { reset } from "@/perfect-seo-shared-components/lib/features/User";
+import { RootState } from "@/perfect-seo-shared-components/lib/store";
 import { loginWithGoogle, logout } from "@/perfect-seo-shared-components/utils/supabase/actions";
 import { useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
 
 const AdminPage = () => {
-  const { isLoggedIn } = useSelector((state: StateTree) => state);
+  const { isLoggedIn } = useSelector((state: RootState) => state);
   const router = useRouter()
   const dispatch = useDispatch()
 
@@ -17,7 +17,7 @@ const AdminPage = () => {
 
         }
         else {
-          dispatch(reduxReset() as any); // Add 'as any' to cast the action to 'UnknownAction'
+          dispatch(reset() as any); // Add 'as any' to cast the action to 'UnknownAction'
           router.push('/')
         }
       })

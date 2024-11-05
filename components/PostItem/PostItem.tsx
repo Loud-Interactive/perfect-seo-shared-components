7c260b87a38c8bdad1d9dcc7f174cf8e0f148f20
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2024-11-05 14:04:15 -0600

    added use client

--- components/PostItem/PostItem.tsx
+++ components/PostItem/PostItem.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useEffect, useState } from "react"
 import TextInput from "../Form/TextInput"
 import { emailValidator } from "@/perfect-seo-shared-components/utils/validators"

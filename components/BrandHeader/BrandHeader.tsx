f1030950ebe827fab5be294245d01a1c7b0e5924
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2024-11-11 12:34:44 -0600

    updated brand header

--- components/BrandHeader/BrandHeader.tsx
+++ components/BrandHeader/BrandHeader.tsx
@@ -5,7 +5,11 @@ import useLogoCheck from "@/perfect-seo-shared-components/hooks/useLogoCheck"
 import { useEffect } from "react"
 import { updateImpression } from "@/perfect-seo-shared-components/services/services"
 
-const BrandHeader = ({ synopsis }) => {
+interface BrandHeaderProps {
+  synopsis: any
+  editable?: boolean;
+}
+const BrandHeader = ({ synopsis, editable = true }: BrandHeaderProps) => {
   const isDark = useLogoCheck(synopsis?.logo_url, synopsis?.domain)
 
   const logoCardClasses = classNames('card p-3 h-100 d-flex align-items-center justify-content-center',
@@ -36,9 +40,9 @@ const BrandHeader = ({ synopsis }) => {
             <div className={logoCardClasses}>
               <div className={styles.logoWrap}>
                 <img src={synopsis?.logo_url} className='w-75' />
-                <div className={styles.logoUpdate}>
+                {editable && <div className={styles.logoUpdate}>
                   <a href={`https://preferencesperfect.ai/domain/${synopsis?.domain}?tab=brand-identity`} className='text-primary' target='_blank'>Update Logo</a>
-                </div>
+                </div>}
               </div>
             </div>
           </div>}
@@ -46,7 +50,8 @@ const BrandHeader = ({ synopsis }) => {
             <h1 className="text-start mb-1"><TypeWriterText string={`Content for ${synopsis?.brand_name || synopsis?.domain}`} withBlink /></h1>
             {synopsis?.synopsis && <div className='card p-3'>
               <div className={styles.synopsisHeader}>
-                <strong>Synopsis</strong>            <a href={`https://preferencesperfect.ai/domain/${synopsis?.domain}`} className={styles.synopsisUpdate} target='_blank'>Update Synopsis</a>
+                <strong>Synopsis</strong>
+                {editable && <a href={`https://preferencesperfect.ai/domain/${synopsis?.domain}`} className={styles.synopsisUpdate} target='_blank'>Update Synopsis</a>}
               </div>
               <p className='mb-0'>{synopsis?.synopsis}</p></div>}
           </div>

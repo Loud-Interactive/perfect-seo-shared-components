43002a35ca568886c4979377b1985864c8333595
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2025-03-04 11:13:00 -0600

    update index modal

--- components/IndexModal/IndexModal.tsx
+++ components/IndexModal/IndexModal.tsx
@@ -1,6 +1,6 @@
 import { PostProps } from "@/perfect-seo-shared-components/data/types";
 import axiosInstance from "@/perfect-seo-shared-components/utils/axiosInstance";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface IndexModalProps {
   post: PostProps;
@@ -11,7 +11,9 @@ const IndexModal = ({ post, onClose }: IndexModalProps) => {
 
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState();
   const clickHandler = (e) => {
+    setErrorMessage(undefined)
     e.preventDefault();
     setLoading(true);
     setSuccess(false)
@@ -23,22 +25,26 @@ const IndexModal = ({ post, onClose }: IndexModalProps) => {
       .then(res => {
         setSuccess(true)
         setLoading(false)
-        onClose();
       })
       .catch(err => {
+        setErrorMessage(err.response.data.message)
         setLoading(false)
       })
   }
 
+  useEffect(() => {
+    setSuccess(false)
+  }, [post])
+
   return (
     <div className="card p-5">
       <h3 className="mb-3">Index Modal</h3>
-      <div className="my-4">
-        {success ? <>
+      <div>
+        {!success ? <>
           <p>{post?.index_status ? 'Post has already been indexed. Would you like to reindex?' : 'Would you like to index this post?'}
           </p>
           <p>
-            <strong>URL</strong>
+            <strong>URL</strong><br />
             <a href={post.live_post_url} target="_blank">{post.live_post_url}</a>
           </p>
         </>
@@ -46,9 +52,14 @@ const IndexModal = ({ post, onClose }: IndexModalProps) => {
           <p>Your post has been succesfully indexed!
           </p>
         }
-        {!success && <div>
-          <button className="btn btn-primary" disabled={loading} onClick={clickHandler}>{loading ? 'Indexing...' : 'Index'}</button>
-        </div>}
+        {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
+        {success ?
+          <div className="mt-3">
+            <button className="btn btn-primary" disabled={loading} onClick={onClose}>close</button>
+          </div>
+          : <div className="mt-3">
+            <button className="btn btn-primary" disabled={loading} onClick={clickHandler}>{loading ? 'Indexing...' : 'Index'}</button>
+          </div>}
       </div>
     </div>
   )

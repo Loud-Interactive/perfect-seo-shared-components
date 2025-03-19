7c910ca925880a6afc23685484dc124e9a6ac9d5
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2025-03-19 15:52:43 -0500

    updates for plan refresh

--- components/PlansList/PlansList.tsx
+++ components/PlansList/PlansList.tsx
@@ -88,7 +88,7 @@ const PlansList = ({ domain_name, active }: PlanListProps) => {
     let interval;
     if (active && !newModal) {
       fetchPlans();
-      interval = setInterval(fetchPlans, 300000)
+      // interval = setInterval(fetchPlans, 300000)
     }
 
     return () => {
@@ -142,7 +142,7 @@ const PlansList = ({ domain_name, active }: PlanListProps) => {
 
   const newCloseHandler = () => {
     setDuplicateInfo(null)
-    // setTimeout(() => fetchPlans(), 60000)
+    setTimeout(() => fetchPlans(), 60000)
     return setNewModal(false)
   }
 

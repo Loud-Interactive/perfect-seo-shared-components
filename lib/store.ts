90148492f76b29d72a96ed0624fdac72b009bbf3
jimmieswaggthorne <jimmieswaggthorne@gmail.com>
2025-06-23 13:38:11 -0500

    added persistant redux

--- lib/store.ts
+++ lib/store.ts
@@ -1,14 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
 import User from './features/User';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
+const persistedReducer = persistReducer(persistConfig, User);
 
 export const makeStore = () => {
   return configureStore({
-    reducer: User
+    reducer: persistedReducer
   })
-
 };
+
+export const persistor = persistStore(makeStore());
 
 // Infer the type of makeStore
 export type AppStore = ReturnType<typeof makeStore>
